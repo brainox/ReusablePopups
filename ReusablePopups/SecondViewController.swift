92ba8c86c3ac6ec389f58aa530c740f69978666c
brainox <macmartins081@gmail.com>
2021-07-18 17:49:37 +0100

    added notification centre to second view controller

--- ReusablePopups/SecondViewController.swift
+++ ReusablePopups/SecondViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
+    var observer: NSObjectProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(forName: .saveDateName, object: nil, queue: OperationQueue.main) { (notification) in
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 1. Notifications: Old Way
+        //        NotificationCenter.default.addObserver(self, selector: #selector(handlePopupClosing), name: .saveDateName, object: nil)
+        
+        // 2. Notifications: New Way
+        observer = NotificationCenter.default.addObserver(forName: .saveDateName, object: nil, queue: OperationQueue.main) { (notification) in
             let dateVC = notification.object as! DatePopupViewController
             self.dateLabel.text = dateVC.formattedDate
         }
     }
-
-
+    // 2. Notifications: New Way
+    //    @objc func handlePopupClosing(notification: Notification) {
+    //        let dateVC = notification.object as! DatePopupViewController
+    //        dateLabel.text = dateVC.formattedDate
+    //    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let observer = observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+    }
 }
