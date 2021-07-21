f8fb557f51e59b085760a64039285fc95458ab5e
brainox <macmartins081@gmail.com>
2021-07-21 09:25:58 +0100

    cleaned up the commented code

--- ReusablePopups/SelectTimeViewController.swift
+++ ReusablePopups/SelectTimeViewController.swift
@@ -11,16 +11,9 @@ class SelectTimeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func selectTime_TouchUpInside(_ sender: Any) {
-//        let storyBoard = UIStoryboard(name: "DatePopupViewController", bundle: nil)
-//        guard let popupVC = storyboard?.instantiateViewController(withIdentifier: "toDatePopupViewControllerSegue")  else { return } //storyboard?.instantiateInitialViewController() else { return}
-//        storyboard?.instantiateViewController(withIdentifier: "datePopupVC")
-//        present(popupVC, animated: true, completion: nil)
-        
         let storyboard = UIStoryboard(name: "DatePopupViewController", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "datePopupVC") as! DatePopupViewController
         controller.showTimePicker = true
